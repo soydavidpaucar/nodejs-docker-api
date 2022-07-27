1a0b78b9846aec36ec9a27b9d9386100bc3341a7
David Paucar <soydavidpaucar@gmail.com>
2022-07-27 16:04:27 -0500

    :rotating_light: fixed linter warnings

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => { 
-		res.send('healthcheck');
-})
+app.get("/", (req, res) => {
+  res.send("<h1>healthcheck</h1>");
+});
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-})
+  console.log(`Server is listening on port ${port}`);
+});
