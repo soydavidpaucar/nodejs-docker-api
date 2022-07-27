8efbd1235fd5d16698ec9527dee45d648a71dac1
David Paucar <soydavidpaucar@gmail.com>
2022-07-27 17:00:58 -0500

    :art: created connection with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express();
+
+mongoose
+  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}`)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("<h1>healthcheck</h1>");
