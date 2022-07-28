d906e60ea2428639511eb37e4548dbbdf7d720d9
David Paucar <soydavidpaucar@gmail.com>
2022-07-27 23:28:45 -0500

    :necktie: created logic to connect with redis and store user cookie session

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+const { createClient } = require("redis");
 const postRouter = require("./routes/post.router");
 const userRouter = require("./routes/user.router");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, SESSION_SECRET, REDIS_HOST } = require("./config/config");
+
+const redisClient = createClient({ legacyMode: true, url: `redis://${REDIS_HOST}` });
+redisClient.connect().then(console.log("redis connected")).catch(console.error);
 
 const app = express();
 app.use(express.json());
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 mongoose
   .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}`)
@@ -15,10 +32,6 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
-app.get("/", (req, res) => {
-  res.send("<h1>healthcheck</h1>");
-});
 
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/users", userRouter);
