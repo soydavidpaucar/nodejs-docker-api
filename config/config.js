23ad5e1ef4d5258be31cef83b20d5c363ab151e1
David Paucar <soydavidpaucar@gmail.com>
2022-07-27 23:27:41 -0500

    :wrench: added session secret and redis host env variable

--- config/config.js
+++ config/config.js
@@ -3,4 +3,6 @@ module.exports = {
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
   MONGO_IP: process.env.MONGO_IP || "mongodb",
   MONGO_PORT: process.env.MONGO_PORT || 27017,
+  REDIS_HOST: process.env.REDIS_HOST || "redis",
+  SESSION_SECRET: process.env.SESSION_SECRET,
 };
