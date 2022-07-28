13f9f611c8b4a3b71baf422a10032ff3afe78433
David Paucar <soydavidpaucar@gmail.com>
2022-07-27 23:28:21 -0500

    :lock: added cookie in header user session

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,6 +9,7 @@ const signUp = async (req, res) => {
   const user = new User({ username, password: encryptedPassword });
   try {
     await user.save();
+    req.session.user = user;
     res.status(201).json({ status: "success", data: { user } });
   } catch (err) {
     res.status(500).json({ status: "error", message: err.message });
@@ -28,6 +29,7 @@ const signIn = async (req, res) => {
     if (!validPassword) {
       res.status(401).json({ status: "error", message: "Invalid password" });
     }
+    req.session.user = user;
     res.status(200).json({ status: "success" });
   } catch (e) {
     res.status(500).json({ status: "error", message: e.message });
